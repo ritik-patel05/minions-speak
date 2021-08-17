f031f065ad54fbfcf7e9c91b013a5f942fc5b64a
unknown <ritikpatel05121999@gmail.com>
2021-08-17 15:35:26 +0530

    feat: added translation and styling

---
+++ app.js
@@ -0,0 +1,19 @@
+let inputTxt = document.querySelector('#txt-input');
+let translateBtn = document.querySelector('#btn-translate');
+let outputDiv = document.querySelector('#output');
+
+function translateString() {
+	console.log('Click happened');
+	let query = inputTxt.value;
+
+	let url =
+		'https://api.funtranslations.com/translate/minion.json?text=' + query;
+
+	console.log(query);
+	fetch(url)
+		.then((response) => response.json)
+		.then((json) => (outputDiv.innerText = json.contents.translated))
+		.catch((error) => alert('Some error occurred'));
+}
+
+translateBtn.addEventListener('click', translateString);
